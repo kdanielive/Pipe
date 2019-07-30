2ccccffee702346c3bf740fa0143cbf4c6b72fde
Daniel Kim <kdanielive@gmail.com>
2019-07-30 13:50:25 +0900

    First files + .gitignore

---
+++ Pipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pipe
+//
+//  Created by Daniel Kim on 7/30/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
