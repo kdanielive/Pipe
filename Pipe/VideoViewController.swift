3e7dea35c877a9d4303230ad5020efa3e44d268e
Daniel Kim <kdanielive@gmail.com>
2019-07-31 10:53:07 +0900

    Added a couple of images / Halt on making custom controls

--- Pipe/VideoViewController.swift
+++ Pipe/VideoViewController.swift
@@ -15,27 +15,46 @@ class VideoViewController: UIViewController {
     var videos = Video.allVideos()
     @IBOutlet var videoContainerView: UIView!
     
+    // Custom controls
+    var playPauseButton: PlayPauseButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Video Player at the top part of the view
         let videoURL = videos[0].url
-        let player = AVPlayer(url: videoURL)
         let playerViewController = AVPlayerViewController()
-        playerViewController.player = player
+        let player = AVPlayer(url: videoURL)
         
+        /*
+        // Custom controls part
+        playerViewController.showsPlaybackControls = false
+        playPauseButton = PlayPauseButton()
+        playPauseButton.avPlayer = player
+        videoContainerView.addSubview(playPauseButton)
+        playPauseButton.setup(in: self)
+        */
+        
+        // Back to implementing video player at the top of the view
+        playerViewController.player = player
         addChildViewController(playerViewController)
         playerViewController.view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         playerViewController.view.frame = videoContainerView.bounds
         videoContainerView.addSubview(playerViewController.view)
         playerViewController.player!.play()
-       
+        
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        playPauseButton.updateUI()
     }
     
 
