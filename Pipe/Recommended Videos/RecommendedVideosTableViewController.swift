1c1ec8d854c031cb303599ce54943524a7943631
Daniel Kim <kdanielive@gmail.com>
2019-08-01 17:25:00 +0900

    Added the comments header

--- Pipe/Recommended Videos/RecommendedVideosTableViewController.swift
+++ Pipe/Recommended Videos/RecommendedVideosTableViewController.swift
@@ -34,7 +34,7 @@ class RecommendedVideosTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 5
     }
 
     
