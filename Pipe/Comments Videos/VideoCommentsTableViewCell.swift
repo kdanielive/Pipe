860353b86b33a09a4325f656f76fe70752104fe6
Daniel Kim <kdanielive@gmail.com>
2019-08-05 09:37:35 +0900

    Saving changes for now

---
+++ Pipe/Comments Videos/VideoCommentsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoCommentsTableViewCell.swift
+//  Pipe
+//
+//  Created by Daniel Kim on 8/2/19.
+//  Copyright © 2019 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class VideoCommentsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
