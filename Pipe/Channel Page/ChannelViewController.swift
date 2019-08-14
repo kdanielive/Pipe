3894892ebad45513a045fbcd419df38467129390
Daniel Kim <kdanielive@gmail.com>
2019-08-14 13:11:44 +0900

    the label stuff is working with the stack view

--- Pipe/Channel Page/ChannelViewController.swift
+++ Pipe/Channel Page/ChannelViewController.swift
@@ -12,15 +12,38 @@ class ChannelViewController: UIViewController {
 
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var contentView: UIView!
+
+    @IBOutlet var labels: [UILabel]!
+    let mockLabelTitles = ["helloooooooo", "slkdjflsk;fhi", "holoholo"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        layoutCategoryLabels(labelTitles: mockLabelTitles)
         
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height)
+    }
+    
+    func layoutCategoryLabels(labelTitles: [String]) {
+        for label in labels {
+            label.sizeToFit()
+        }
+        
+        // Setting label text
+        labels[1].text = mockLabelTitles[0]
+        labels[2].text = mockLabelTitles[1]
+        labels[3].text = mockLabelTitles[2]
+        
+        // Hiding labels without text from view
+        for label in labels{
+            if label.text == "Label" {
+                label.isHidden = true
+            }
+        }
     }
 
     /*
