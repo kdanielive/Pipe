38ce9b2c2e8f371845d684b15828fe5cbf128a97
Daniel Kim <kdanielive@gmail.com>
2019-08-14 10:44:52 +0900

    Working on setting the channel page

--- Pipe/Channel Page/ChannelViewController.swift
+++ Pipe/Channel Page/ChannelViewController.swift
@@ -12,10 +12,10 @@ class ChannelViewController: UIViewController {
 
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
     override func viewDidLayoutSubviews() {
@@ -25,13 +25,12 @@ class ChannelViewController: UIViewController {
 
     /*
     // MARK: - Navigation
-
+     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     */
-    
 
 }
