2461f3531341a723e7f6162231d162ed946e6f3c
Daniel Kim <kdanielive@gmail.com>
2019-08-21 09:51:35 +0900

    Setup slider finished

--- Pipe/New Video Page/VideoViewController2.swift
+++ Pipe/New Video Page/VideoViewController2.swift
@@ -11,14 +11,23 @@ import AVFoundation
 import AVKit
 
 class VideoViewController2: UIViewController {
-
-
-    @IBOutlet var scrollView: UIScrollView!
-    @IBOutlet var contentView: UIView!
     
+    
+    //// MacroViews: scrollView, contentView, backgroundView
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    @IBOutlet var backgroundView: UIView!
+    
+    //// Video Views
     let videoContainerView = UIView()
     let playerViewController = CustomAVPlayerViewController()
     var player = AVPlayer(url: Video.allVideos()[4].url)
+    
+    //// Video components
+    // Slider
+    var timeObserver: Any?
+    let timeRemainingLabel = UILabel()
+    let progressSlider = UISlider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +39,91 @@ class VideoViewController2: UIViewController {
         playerViewController.showsPlaybackControls = false
         
         // Implementing video player at the top of the view
+        loadVideoIntoContainerView()
+
+        //// Set up components in the containerView
+        setupSlider()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupPlayButton() {
+        
+    }
+    
+    func setupSlider() {
+        let sliderX = CGFloat(0)
+        let sliderY = videoContainerView.frame.height
+        let sliderWidth = videoContainerView.frame.width
+        let sliderHeight = CGFloat(10)
+        
+        progressSlider.minimumTrackTintColor = UIColor.blue
+        progressSlider.maximumTrackTintColor = UIColor.gray
+        progressSlider.thumbTintColor = UIColor.blue
+        progressSlider.maximumValue = 1.0
+        progressSlider.minimumValue = 0
+        progressSlider.setValue(0, animated: false)
+        progressSlider.frame = CGRect(x: sliderX, y: sliderY - sliderHeight/2, width: sliderWidth, height: sliderHeight)
+        setSliderThumbTintColor(UIColor.blue)
+        
+        backgroundView.addSubview(progressSlider)
+        backgroundView.bringSubview(toFront: progressSlider)
+        
+        progressSlider.addTarget(self, action: #selector(playbackSliderValueChanged), for: .valueChanged)
+        
+        let interval = CMTime(seconds: 0.01, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        timeObserver = player.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main, using: { elapsedTime in
+            self.updateVideoPlayerState()
+        })
+        timeRemainingLabel.textColor = UIColor.white
+        timeRemainingLabel.font = UIFont.boldSystemFont(ofSize: 16.0)
+    }
+    
+    func setSliderThumbTintColor(_ color: UIColor) {
+        let circleImage = makeCircleWith(size: CGSize(width: 14, height: 14),
+                                         backgroundColor: color)
+        progressSlider.setThumbImage(circleImage, for: .normal)
+        progressSlider.setThumbImage(circleImage, for: .highlighted)
+    }
+    
+    @objc func playbackSliderValueChanged(sender: UISlider) {
+        let duration = player.currentItem!.duration
+        let value = Float64(progressSlider.value) * CMTimeGetSeconds(duration)
+        let seekTime = CMTime(value: CMTimeValue(value), timescale: 1)
+        player.seek(to: seekTime )
+    }
+
+    
+    func updateVideoPlayerState() {
+        let currentTime = player.currentTime()
+        let currentTimeInSeconds = CMTimeGetSeconds(currentTime)
+        progressSlider.value = Float(currentTimeInSeconds)
+        if let currentItem = player.currentItem {
+            let duration = currentItem.duration
+            if (CMTIME_IS_INVALID(duration)) {
+                return;
+            }
+            let currentTime = currentItem.currentTime()
+            progressSlider.value = Float(CMTimeGetSeconds(currentTime) / CMTimeGetSeconds(duration))
+            
+            // Update time remaining label
+            let totalTimeInSeconds = CMTimeGetSeconds(duration)
+            let remainingTimeInSeconds = totalTimeInSeconds - currentTimeInSeconds
+            
+            let mins = remainingTimeInSeconds / 60
+            let secs = remainingTimeInSeconds.truncatingRemainder(dividingBy: 60)
+            let timeformatter = NumberFormatter()
+            timeformatter.minimumIntegerDigits = 2
+            timeformatter.minimumFractionDigits = 0
+            timeformatter.roundingMode = .down
+            guard let minsStr = timeformatter.string(from: NSNumber(value: mins)), let secsStr = timeformatter.string(from: NSNumber(value: secs)) else {
+                return
+            }
+            timeRemainingLabel.text = "\(minsStr):\(secsStr)"
+        }
+    }
+    
+    func loadVideoIntoContainerView () {
         playerViewController.player = player
         addChildViewController(playerViewController)
         playerViewController.view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
@@ -37,11 +131,6 @@ class VideoViewController2: UIViewController {
         //playerViewController.videoGravity = "resize"
         videoContainerView.addSubview(playerViewController.view)
         playerViewController.player!.play()
-
-
-        
-
-        // Do any additional setup after loading the view.
     }
     
     func setupVideoContainerView() {
@@ -49,13 +138,15 @@ class VideoViewController2: UIViewController {
         let videoContainerViewHeight = view.frame.width * (211/375)
         videoContainerView.frame = CGRect(x: 0, y: 0, width: videoContainerViewWidth, height: videoContainerViewHeight)
         
-        view.addSubview(videoContainerView)
-        
+        backgroundView.addSubview(videoContainerView)
     }
+    
+
     
     override func viewDidLayoutSubviews() {
         
     }
+    
     
 
     /*
@@ -67,6 +158,19 @@ class VideoViewController2: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    fileprivate func makeCircleWith(size: CGSize, backgroundColor: UIColor) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(backgroundColor.cgColor)
+        context?.setStrokeColor(UIColor.clear.cgColor)
+        let bounds = CGRect(origin: .zero, size: size)
+        context?.addEllipse(in: bounds)
+        context?.drawPath(using: .fill)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
     
     class CustomAVPlayerViewController: AVPlayerViewController {
         var screenTouchCount = 0
