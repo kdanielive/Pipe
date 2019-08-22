d3c2ad2aac07253c6928cb1e6d50ff08868562d2
Daniel Kim <kdanielive@gmail.com>
2019-08-22 10:31:39 +0900

    Fixed the category view error

--- Pipe/Category Page/HorizontalCategoryTableViewCell2.swift
+++ Pipe/Category Page/HorizontalCategoryTableViewCell2.swift
@@ -39,18 +39,18 @@ class HorizontalCategoryTableViewCell2: UITableViewCell, UICollectionViewDataSou
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
             
-        return CGSize(width: 251, height: 224)
-
+        return CGSize(width: 251, height: 227)
     }
- 
     
     override func layoutSubviews() {
         collectionView.dataSource = self
         
+        /*
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.minimumInteritemSpacing = 20
         layout.minimumLineSpacing = 20
         collectionView.collectionViewLayout = layout
+        */
     }
     
     
